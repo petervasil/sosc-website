b23a8d91a81090b459e64ce3d8afdf134818db54
Musthaq Ahamad <musthu.gm@gmail.com>
2018-08-05 00:10:28 +0530

    Updates

---
+++ path---team-a969528664f410513810.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xb326e24804c6],{358:function(a,e){a.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{username:"haxzie",name:"Musthaq Ahamad",designation:"Community Lead"}}},{node:{frontmatter:{username:"rumaan",name:"Rumaan Khalander",designation:"Technical Lead"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---team-a969528664f410513810.js.map
