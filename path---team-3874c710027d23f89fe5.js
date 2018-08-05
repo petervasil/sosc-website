5509a06ee5bdc59ddf15a5905129f1ca1ce51b8e
Musthaq Ahamad <musthu.gm@gmail.com>
2018-08-05 22:10:05 +0530

    Updates

---
+++ path---team-3874c710027d23f89fe5.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xb326e24804c6],{358:function(a,e){a.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{username:"nathanielmathew",name:"Nathaniel Ryan Mathew",designation:"Co-Head of Design"}}},{node:{frontmatter:{username:"shravan1998",name:"Shravan",designation:"Co-Web Admin"}}},{node:{frontmatter:{username:"haxzie",name:"Musthaq Ahamad",designation:"Community Lead"}}},{node:{frontmatter:{username:"rumaan",name:"Rumaan Khalander",designation:"Technical Lead"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---team-3874c710027d23f89fe5.js.map
