56b9f69d7bffae3730a71bcc79c06218de17e537
Peter Vasiľ <peter.vasil@student.tuke.sk>
2018-12-02 09:36:05 +0100

    Example

--- selenium-tests/src/test/java/tests/AllTests.java
+++ selenium-tests/src/test/java/tests/AllTests.java
@@ -1,8 +1,14 @@
 package tests;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllTests {
 
@@ -18,6 +24,12 @@ public class AllTests {
     @Test
     public void testTitle() {
         driver.get("http://google.com");
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 5000);
+
+        WebElement element = driver.findElement(By.xpath(""));
+
+        webDriverWait.until(ExpectedConditions.visibilityOf(element)).click();
+
         System.out.println(driver.getTitle());
     }
 
